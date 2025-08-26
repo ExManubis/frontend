98f3da1495d8c7ad670152461a040b29cfd56c14
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-08-26 13:15:25 +0200

    style(index): style game
    Styled the game.

--- guess_number/js/numbergame.js
+++ guess_number/js/numbergame.js
@@ -2,30 +2,37 @@
 
 // Get form
 const guessForm = document.getElementById("guessForm");
-const numberGuessed = document.getElementById("guessInput")
+const numberGuessed = document.getElementById("guessInput");
 
 // Get result section
-const resultSection = document.getElementById("result")
+const yourGuess = document.getElementById("yourGuess");
+const resultSection = document.getElementById("result");
+const winGif = document.getElementById("winGif");
+const score = document.getElementById("score");
 
 // Generate number
 const randomNumber = Math.round(Math.random() * 100);
 
-// Get submitted number
-guessForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let numberInput = numberGuessed.value;
-    if (numberInput == randomNumber) {
-        resultSection.innerText = "Jaaa"
-    };
-    if (numberInput > randomNumber) {
-        resultSection.innerText = "For højt!"
-    }
-    if (numberInput < randomNumber) {
-        resultSection.innerText = "For lavt!"
-    }
-    console.log(numberInput)
-    return numberInput
-})
+// Set score
+let Yourscore = 1;
 
-// Debug
-console.log(randomNumber);
+// Get submitted number
+guessForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let numberInput = numberGuessed.value;
+  yourGuess.innerText = `Du gættede på: ${numberInput}`;
+  score.innerText = `Antal gæt: ${Yourscore.toString()}`;
+  if (numberInput == randomNumber) {
+    resultSection.innerText = "Tillykke! Du gættede rigtigt!";
+    winGif.style.display = "block";
+  }
+  if (numberInput > randomNumber) {
+    resultSection.innerText = "For højt. Prøv igen!";
+  }
+  if (numberInput < randomNumber) {
+    resultSection.innerText = "For lavt. Prøv igen!";
+  }
+  Yourscore++;
+  return numberInput;
+});
+
