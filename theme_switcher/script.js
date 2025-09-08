e3fb0559331656a37e838d2a378afb8bc963593a
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-09-08 13:26:51 +0200

    commit

---
+++ theme_switcher/script.js
@@ -0,0 +1,8 @@
+const selector = document.getElementById('theme-switcher')
+
+
+selector.addEventListener('change', (e) => {
+    let theme = e.target.value
+    document.querySelector('body').dataset.theme = theme
+    console.log(theme)
+})
