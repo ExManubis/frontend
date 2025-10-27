be31502bd559861f8d999aea342ccac7357faf5b
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-10-27 16:50:05 +0100

    commit

--- like-button/3/script.js
+++ like-button/3/script.js
@@ -2,10 +2,9 @@ const buttons = document.querySelectorAll('.fa-thumbs-up');
 
 document.addEventListener('DOMContentLoaded', () => {
 	buttons.forEach((button) => {
-		button.parentNode.dataset.clicks = '0';
+		button.parentNode.dataset.clicks = '1';
 		button.parentNode.addEventListener('click', (e) => {
-			e.currentTarget.dataset.clicks++;
-			clicks = e.currentTarget.dataset.clicks;
+			clicks = e.currentTarget.dataset.clicks++;
 			e.currentTarget.innerHTML = `<i class="fa fa-thumbs-up"></i>${clicks > 1 ? ' Likes' : ' Like'} ${clicks}`;
 		});
 	});
